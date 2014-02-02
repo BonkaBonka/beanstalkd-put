276d65c21ae74bb8f18e5576f6cd4645c9fb3015
James Cook <jamesc@blueshirt2.lan>
2014-02-02 18:30:16 -0500

    Add command-line options to allow full customization.

--- bsc.c
+++ bsc.c
@@ -1,35 +1,97 @@
+#include <getopt.h>
 #include <limits.h>
 #include <stdio.h>
 
 #include "beanstalk.h"
 
+char *tube_name = "default";
+char *server_host = "localhost";
+int server_port = 11300;
+// Priority 0 is max, UINT32_MAX is lowest
+// See: https://github.com/kr/beanstalkd/blob/v1.3/doc/protocol.txt#l135
+uint32_t job_priority = UINT32_MAX;
+uint32_t job_delay = 0;
+uint32_t job_ttr = 120;
+
+void display_help()
+{
+	puts("Usage:");
+	puts("  bsc [options] <message>");
+	puts("");
+	puts("Options:");
+	puts("  -t <tube>       which tube to put the job into");
+	puts("  -s <server>     the beanstalk server hostname");
+	puts("  -p <port>       the beanstalk server port number");
+	puts("  -P <priority>   the job priority");
+	puts("  -D <delay>      how long to delay making the job available");
+	puts("  -T <ttr>        how long to wait before returning the job to the queue");
+	puts("  -h              display this help");
+}
+
+int process_args(int argc, char **argv)
+{
+	int c;
+
+	while((c = getopt(argc, argv, "t:s:p:P:D:T:h")) != -1)
+	{
+		switch(c)
+		{
+			case 't':
+				tube_name = optarg;
+				break;
+			case 's':
+				server_host = optarg;
+				break;
+			case 'p':
+				server_port = atoi(optarg);
+				break;
+			case 'P':
+				job_priority = atoi(optarg);
+				break;
+			case 'D':
+				job_delay = atoi(optarg);
+				break;
+			case 'T':
+				job_ttr = atoi(optarg);
+				break;
+			default:
+				display_help();
+				return -1;
+		}
+	}
+
+	if(optind >= argc || optind + 1 < argc)
+	{
+		display_help();
+		return -1;
+	}
+
+	return optind;
+}
+
 int main(int argc, char **argv) {
-	if (argc != 3) {
-		fprintf(stderr, "Usage: %s <tube> <message>\n", argv[0]);
+	int c = process_args(argc, argv);
+	if(c < 1)
+	{
 		return 1;
 	}
 
-	int socket = bs_connect("127.0.0.1", 11300);
+	int socket = bs_connect(server_host, server_port);
 	if (socket == BS_STATUS_FAIL) {
-		fputs("Unable to connect to beanstalk\n", stderr);
+		fprintf(stderr, "Unable to connect to beanstalk %s:%d\n", server_host, server_port);
 		return 2;
 	}
 
-	if (bs_use(socket, argv[1]) == BS_STATUS_FAIL) {
+	if (bs_use(socket, tube_name) == BS_STATUS_FAIL) {
 		bs_disconnect(socket);
-		fprintf(stderr, "Unable to use beanstalk tube %s\n", argv[1]);
+		fprintf(stderr, "Unable to use beanstalk tube %s\n", tube_name);
 		return 3;
 	}
 
-	// Priority 0 is max, UINT32_MAX is lowest
-	// See: https://github.com/kr/beanstalkd/blob/v1.3/doc/protocol.txt#l135
-	uint32_t priority = UINT32_MAX;
-	uint32_t delay = 0;
-	uint32_t ttr = 600;
-	int64_t id = bs_put(socket, priority, delay, ttr, argv[2], strlen(argv[2]));
+	int64_t id = bs_put(socket, job_priority, job_delay, job_ttr, argv[c], strlen(argv[c]));
 	if (id == 0) {
 		bs_disconnect(socket);
-		fprintf(stderr, "Unable to put message into tube %s\n", argv[1]);
+		fprintf(stderr, "Unable to put message into tube %s\n", argv[c]);
 		return 4;
 	}
 
